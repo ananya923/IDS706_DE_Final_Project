969133febc8505631085875e9dd278108d2f2ac9
Farnoosh Memari <farnooshmemari72@gmail.com>
2025-12-05 18:56:53 -0500

    Change database engine from Postgres to MySQL

--- dashboard/app.py
+++ dashboard/app.py
@@ -25,8 +25,8 @@ def load_real_data():
 
     # Postgres engine (change if MySQL)
     engine = create_engine(
-        f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-    )
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 
     query = """
         SELECT
